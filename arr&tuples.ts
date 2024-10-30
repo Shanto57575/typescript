87e646c8f425859a51cd5217b1363f0f44798467
Sh@nto <shanto57575@gmail.com>
2024-10-30 22:45:38 +0600

    Create arr&tuples.ts

---
+++ arr&tuples.ts
@@ -0,0 +1,10 @@
+let arr: number[] = [1, 2, 3, 4, 5]
+console.log("arr: " + arr.length)
+
+let myTuples: [string, number] = ['shanto', 12]
+console.log(myTuples)
+
+let myTuples2: [number, number, number, string, undefined, null] = [1, 2, 3, "Four", undefined, null]
+
+let myTuples3: [number, string] = [1, 'two']
+
