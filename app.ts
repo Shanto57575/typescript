4e1bfb6214fcd712df14b44a3baa627aeeca20dd
Shanto57575 <shanto57575@gmail.com>
2024-10-30 21:11:11 +0600

    started typescript

---
+++ app.ts
@@ -0,0 +1,15 @@
+// premitive types vs refrence types
+
+// if (), {}, [] this bracket are there then its reference types else
+// else its primitive types
+
+/*
+For exmaples
+
+lets greeeting: string = "Hello"
+let newGreeting = greeeting
+
+greeting = "Hi"
+console.log(newgreeting) // HELLO
+
+*/
